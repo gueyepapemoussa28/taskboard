042f950f64bb3f78897c8bb483d05b3ac9ea13ba
Moussa <moussa@192.168.1.27>
2026-04-11 22:18:32 +0000

    fix: correct Supabase project URL and API key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://cfegswppqthtdrwegoc.supabase.co',
+  'https://cfegswppqtthtdrwegoc.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmZWdzd3BwcXR0aHRkcndlZ29jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MzI0ODgsImV4cCI6MjA5MTQwODQ4OH0.Il8yoT-7n9ocgG9D0_PqFGjoDUmnX2cELcgrfnS7ieY'
 )
